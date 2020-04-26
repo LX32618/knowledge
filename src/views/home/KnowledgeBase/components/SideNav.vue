b70307a1069ccc7c305f9ccb4fc9cc238d172f59
scuxiatian <scuxiatian@foxmail.com>
2020-04-26 10:40:55 +0800

    knowldgeBaseNav

---
+++ src/views/home/KnowledgeBase/components/SideNav.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-card>
+    <span slot="header">SideNav</span>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'KnowledgeBaseSideNav'
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/home/KnowledgeBase/index.vue
+++ src/views/home/KnowledgeBase/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="container" v-if="currentKnowledgeBase" v-loading="isLoading">
-    <div class="nav-container">
-      <i class="el-icon-location"> </i>
-      <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>{{
-          this.currentKnowledgeBase.categoryName
-        }}</el-breadcrumb-item>
-      </el-breadcrumb>
+    <div class="wrapper">
+      <!-- 面包屑导航条 -->
+      <div class="nav-container">
+        <i class="el-icon-location"></i> 当前位置：
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }" class="breadcurmb-text"
+            >首页</el-breadcrumb-item
+          >
+          <el-breadcrumb-item class="breadcurmb-text">{{
+            this.currentKnowledgeBase.categoryName
+          }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <side-nav></side-nav>
+        </el-col>
+        <el-col :span="18">
+          <detail-area></detail-area>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -15,8 +28,15 @@
 <script>
 import { mapGetters } from 'vuex'
 import { errorMsg } from '@/utils/msg'
+import SideNav from './components/SideNav'
+import DetailArea from './components/DetailArea'
+
 export default {
   name: 'KnowledgeBase',
+  components: {
+    SideNav,
+    DetailArea
+  },
   props: {
     id: {
       type: String,
@@ -58,13 +78,19 @@ export default {
   background-color: #ecf0f1;
   margin-top: 90px;
 }
-.nav-container {
+.wrapper {
   width: 1200px;
   margin: 0 auto;
   height: 100%;
-  padding: 5px 0;
+  padding: 10px 0;
+  font-size: 14px;
+  line-height: 1;
+  font-family: "PingFang SC";
 }
 .el-breadcrumb {
   display: inline-block;
+}
+.nav-container {
+  margin-bottom: 10px;
 }
 </style>
