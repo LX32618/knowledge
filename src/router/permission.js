c84ec23c2535fc12b094d1fb51d977ba317391fc
scuxiatian <scuxiatian@foxmail.com>
2020-05-25 11:09:58 +0800

    fix

--- src/router/permission.js
+++ src/router/permission.js
@@ -7,35 +7,25 @@ const routerGuide = async (to, from, next) => {
 
   const token = getToken()
   // 已登录
-  if (token) {
+  if (token && token !== 'undefined') {
     if (to.path === '/login') {
       next({ path: '/' })
     } else {
       let user = store.getters.userInfo
       if (!user) {
-        user = await store.dispatch('user/getInfo')
+        try {
+          user = await store.dispatch('user/getInfo')
+        } catch (_) {
+          return next({ path: '/login' })
+        }
       }
-      if (!user) {
-        next({ path: '/login' })
-      }
-      // const routes = store.getters.routes
-      // if (!routes || routes.length === 0) {
-      //   store.commit('app/SET_ROUTES')
-      // }
-      // // 判断用户权限
-      // if (to.meta.admin && role !== 0) {
-      //   next('/noPerimission')
-      // } else {
-      //   next()
-      // }
-      next()
     }
     // 未登录
   } else {
     if (to.path === '/login') {
-      next()
+      return next()
     } else {
-      next(`/login?redirect=${to.path}`)
+      return next(`/login?redirect=${to.path}`)
     }
   }
   next()
