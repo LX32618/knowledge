310e9b7eba33851c7f20f6e0a80b8bc4685359fb
LX32618 <45353285+LX32618@users.noreply.github.com>
2021-12-01 14:43:17 +0800

    update

--- src/views/bms/PushManagement/index.vue
+++ src/views/bms/PushManagement/index.vue
@@ -1,32 +1,39 @@
 <template>
-   <!-- <div style="display: flex;flex-direction: row;justify-content: center;height:100%;min-height: 100vh">
-        <div style="display:flex;flex-direction: column;justify-content: center">
-           <div>
-               <span>是否开启知识推送：</span>
-               <el-switch v-model="pushFlag">
-               </el-switch>
-               <el-button type="primary" size="mini" style="float:right">保存</el-button>
-           </div>
-            <div>
-
-            </div>
-        </div>
-    </div>-->
     <div style="margin-top: 20px;min-height: 100vh">
         <span>是否开启知识推送：</span>
         <el-switch v-model="pushFlag">
         </el-switch>
-        <el-button type="primary" size="mini" style="margin-left: 30px;">保存</el-button>
+        <el-button type="primary" size="mini" style="margin-left: 30px;" @click="saveEvent">保存</el-button>
     </div>
 </template>
 
 <script>
+
+    import {fetchPushSwitch,updatePushSwitch} from "@/api/pushManagement.js"
+
     export default {
         name: "PushManagement",
         data(){
             return{
                 pushFlag:false
             }
+        },
+        methods:{
+            async saveEvent(){
+                let pushValue = this.pushFlag?"true":"false";
+                let option = {id:"pushSwitch",pushValue:pushValue};
+                let resp = await updatePushSwitch(option);
+                if(resp.status == "success")
+                {
+                    this.$success("保存成功");
+                }
+            }
+        },
+        async mounted() {
+            let option = {id:"pushSwitch"};
+            let resp = await fetchPushSwitch(option);
+            this.pushFlag = resp.content.pushValue == "true";
+            console.log(resp);
         }
     }
 </script>
