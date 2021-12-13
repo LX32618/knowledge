8f31768e09e8fe9f1085845501e149991a4abfa4
scuxiatian <scuxiatian@foxmail.com>
2021-12-13 17:18:09 +0800

    Fix: Knowledge Comment Empty Content

--- src/views/home/KnowledgeDetail/components/KnowledgeComment.vue
+++ src/views/home/KnowledgeDetail/components/KnowledgeComment.vue
@@ -5,20 +5,53 @@
         <span slot="header">大家对该知识的评论（共 {{ total }} 条）</span>
         <!-- 评论列表 -->
         <a-comment
-          v-for="comment of pagedComments" :key="comment.id"
-          :author="`${comment.userName}${comment.replayName ? ' 回复 ' + comment.replayName : ''}`"
+          v-for="comment of pagedComments"
+          :key="comment.id"
+          :author="
+            `${comment.userName}${
+              comment.replayName ? ' 回复 ' + comment.replayName : ''
+            }`
+          "
           :content="comment.comment"
-          :datetime="comment.createTime">
+          :datetime="comment.createTime"
+        >
           <span slot="avatar">
             <el-avatar icon="el-icon-user"></el-avatar>
           </span>
           <template slot="actions">
-            <el-input v-show="comment.id === activeCommentId" size="mini" v-model="replyContent" />
-            <span v-show="comment.id !== activeCommentId" @click="activeCommentId = comment.id">回复</span>
-            <span class="reply-btn" v-show="comment.id === activeCommentId" @click="handleReply(comment.userId)">确定</span>
-            <span v-show="comment.id === activeCommentId" @click="activeCommentId = null">取消</span>
-            <el-popconfirm title="确定要删除评论?" @onConfirm="handleDelete(comment.id)">
-              <span slot="reference" v-if="comment.userId === userInfo.id && comment.id !== activeCommentId">删除</span>
+            <el-input
+              v-show="comment.id === activeCommentId"
+              size="mini"
+              v-model="replyContent"
+            />
+            <span
+              v-show="comment.id !== activeCommentId"
+              @click="activeCommentId = comment.id"
+              >回复</span
+            >
+            <span
+              class="reply-btn"
+              v-show="comment.id === activeCommentId"
+              @click="handleReply(comment.userId)"
+              >确定</span
+            >
+            <span
+              v-show="comment.id === activeCommentId"
+              @click="activeCommentId = null"
+              >取消</span
+            >
+            <el-popconfirm
+              title="确定要删除评论?"
+              @onConfirm="handleDelete(comment.id)"
+            >
+              <span
+                slot="reference"
+                v-if="
+                  comment.userId === userInfo.id &&
+                    comment.id !== activeCommentId
+                "
+                >删除</span
+              >
             </el-popconfirm>
           </template>
         </a-comment>
@@ -26,7 +59,8 @@
           layout="total, prev, pager, next, jumper"
           :page-size="10"
           :current-page.sync="currentPage"
-          :total="total" />
+          :total="total"
+        />
         <!-- 评论输入区域 -->
         <a-comment>
           <span slot="avatar">
@@ -38,7 +72,13 @@
                 <el-input type="textarea" v-model="commentContent" />
               </el-form-item>
               <el-form>
-                <el-button type="primary" icon="fa fa-file-text" @click="handleComment"> 发表</el-button>
+                <el-button
+                  type="primary"
+                  icon="fa fa-file-text"
+                  @click="handleComment"
+                >
+                  发表</el-button
+                >
               </el-form>
             </el-form>
           </span>
@@ -71,21 +111,25 @@ import 'quill/dist/quill.bubble.css'
 
 import { mapGetters } from 'vuex'
 import { quillEditor } from 'vue-quill-editor'
-import { findCommentsByKnowledgeId, saveComment, deleteComment } from '@/api/knowledgeComment'
+import {
+  findCommentsByKnowledgeId,
+  saveComment,
+  deleteComment
+} from '@/api/knowledgeComment'
 
 export default {
   name: 'KnowledgeComment',
   components: {
     quillEditor
   },
-  data () {
+  data() {
     return {
       ranks: ['好', '一般', '不好'],
       knowledgeAssess: {},
       commentContent: '',
       replyContent: '',
       editorOption: {
-        placeholder: '请输入评论',
+        placeholder: '请输入评论'
       },
       comments: [],
       activeCommentId: null,
@@ -93,24 +137,26 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      'userInfo'
-    ]),
-    total () {
+    ...mapGetters(['userInfo']),
+    total() {
       return this.comments.length
     },
-    pagedComments () {
+    pagedComments() {
       const currentPage = this.currentPage
       return this.comments.slice((currentPage - 1) * 10, currentPage * 10)
     }
   },
   methods: {
-    update () {
-      findCommentsByKnowledgeId(this.$route.params.id).then((res) => {
-        this.comments = res.content
+    update() {
+      findCommentsByKnowledgeId(this.$route.params.id).then(res => {
+        if (res.content === 'true') {
+          this.comments = []
+        } else {
+          this.comments = res.content
+        }
       })
     },
-    handleComment () {
+    handleComment() {
       if (!this.commentContent) {
         this.$error('评论内容不能为空')
         return
@@ -123,7 +169,7 @@ export default {
         this.update()
       })
     },
-    handleReply (replyId) {
+    handleReply(replyId) {
       if (!this.replyContent) {
         this.$error('回复内容不能为空')
         return
@@ -138,14 +184,14 @@ export default {
         this.update()
       })
     },
-    handleDelete (id) {
+    handleDelete(id) {
       deleteComment(id).then(() => {
         this.$success('删除成功')
         this.update()
       })
     }
   },
-  mounted () {
+  mounted() {
     this.update()
   }
 }
