01bd82fa8fff3d1858b9c4f8caa84e070d87eb25
scuxiatian <scuxiatian@foxmail.com>
2021-12-14 10:17:16 +0800

    Fix: Knowledge Collect To Detail Id

--- src/views/home/PersonCenter/components/MyApplication/index.vue
+++ src/views/home/PersonCenter/components/MyApplication/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     <el-tabs v-model="currentTab" type="card" @tab-click="handleTab">
-      <el-tab-pane v-for="tab of tabs" :key="tab.name" :name="tab.name" :label="tab.label"></el-tab-pane>
+      <el-tab-pane
+        v-for="tab of tabs"
+        :key="tab.name"
+        :name="tab.name"
+        :label="tab.label"
+      ></el-tab-pane>
     </el-tabs>
     <!-- 申请知识列表 -->
-    <dynamic-table v-loading="isLoading" :data="applicationKnowledge" :columns="columns">
+    <dynamic-table
+      v-loading="isLoading"
+      :data="applicationKnowledge"
+      :columns="columns"
+    >
       <template v-slot:docName="{ scope }">
-        <el-link class="table-knowledge-link" type="primary" size="sm" @click="viewDetail(scope.row.id)">{{ scope.row.docName }}</el-link>
+        <el-link
+          class="table-knowledge-link"
+          type="primary"
+          size="sm"
+          @click="viewDetail(scope.row.docId)"
+          >{{ scope.row.docName }}</el-link
+        >
       </template>
     </dynamic-table>
     <el-pagination
@@ -14,7 +29,8 @@
       :total="total"
       :current-page.sync="page"
       @current-change="pageChange"
-      layout="total, prev, pager, next, jumper">
+      layout="total, prev, pager, next, jumper"
+    >
     </el-pagination>
   </div>
 </template>
@@ -30,17 +46,17 @@ export default {
   components: {
     DynamicTable
   },
-  data () {
+  data() {
     return {
       page: 1,
       rows: 10,
       total: 0,
       currentTab: 'all',
       tabs: [
-        { name: 'all', label: '全部'},
-        { name: '0', label: '待审核'},
-        { name: '3', label: '未通过'},
-        { name: '1', label: '通过'}
+        { name: 'all', label: '全部' },
+        { name: '0', label: '待审核' },
+        { name: '3', label: '未通过' },
+        { name: '1', label: '通过' }
       ],
       isLoading: false,
       applicationKnowledge: [],
@@ -56,7 +72,7 @@ export default {
         {
           label: '申请时间',
           key: 'time',
-          formatter (row) {
+          formatter(row) {
             return dateTime(row.time)
           }
         },
@@ -71,7 +87,7 @@ export default {
         {
           label: '状态',
           key: 'status',
-          formatter (row) {
+          formatter(row) {
             const statuses = ['待审核', '通过', '', '未通过']
             return statuses[row.status]
           }
@@ -80,18 +96,16 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      'userInfo'
-    ]),
-    currentTabValue () {
+    ...mapGetters(['userInfo']),
+    currentTabValue() {
       return this.currentTab === 'all' ? undefined : this.currentTab
     }
   },
   methods: {
-    pageChange () {
+    pageChange() {
       this.updateApplicationKnowledge()
     },
-    updateApplicationKnowledge () {
+    updateApplicationKnowledge() {
       this.isLoading = true
       getSharedKnowledge({
         page: this.page,
@@ -104,10 +118,10 @@ export default {
         this.total = res.content.total
       })
     },
-    handleTab () {
+    handleTab() {
       this.updateApplicationKnowledge()
     },
-    viewDetail (id) {
+    viewDetail(id) {
       console.log(id)
       const routeData = this.$router.resolve({
         name: 'knowledgeDetail',
@@ -118,11 +132,10 @@ export default {
       window.open(routeData.href, '_blank')
     }
   },
-  mounted () {
+  mounted() {
     this.updateApplicationKnowledge()
   }
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
